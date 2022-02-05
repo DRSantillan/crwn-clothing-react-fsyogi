26adc304fab988b8b16166dab7679088c2a07dc0
Dean <dean@deansantillan.com>
2022-02-05 15:36:15 +1000

    tasks(): installed react-router-dom so that we can access url parameters to stop prop drilling. updated menuitem to use with routerHOC

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,12 @@
 import '../containers/App.css';
-
+import {Switch, Route} from 'react-router-dom'
 import HomePage from '../pages/home/HomePage.component';
 function App() {
-	return <div><HomePage/></div>;
+	return <div>
+		<Switch>
+			<Route exact path='/' component={HomePage}/>
+		</Switch>
+	</div>;
 }
 
 export default App;

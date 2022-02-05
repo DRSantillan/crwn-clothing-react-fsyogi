42a933c49df4da87d9161e9ecdf4a9dd23dc0c04
Dean <dean@deansantillan.com>
2022-02-05 16:19:46 +1000

    task(): added header page using link from react router dom package.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import '../containers/App.css';
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 import HomePage from '../pages/home/HomePage.component';
 import ShopPage from '../pages/shop/ShopPage.component';
+import Header from '../components/header/Header.component';
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
